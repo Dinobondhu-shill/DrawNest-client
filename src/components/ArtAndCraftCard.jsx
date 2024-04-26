40b157bbc2f65532554dc8eab02e9560a08e4fc9
Dinobondhu-shill <dinu.webdev@gmail.com>
2024-04-27 03:18:02 +0600

    art and craft section added

---
+++ src/components/ArtAndCraftCard.jsx
@@ -0,0 +1,31 @@
+import { Link } from "react-router-dom";
+import { GrMoney } from "react-icons/gr";
+import { GiTimeTrap } from "react-icons/gi";
+import { IoStarHalf } from "react-icons/io5";
+import { SiShutterstock } from "react-icons/si";
+import { FaLocationArrow } from "react-icons/fa6";
+
+const ArtAndCraftCard = ({item}) => {
+  const {image, item_name, price, processing_time, rating, stockStatus } = item
+  return (
+    <div className="border p-3 rounded-md hover:scale-105 ">
+      <div>
+        <img src={image} alt="" className="w-full max-h-[300px] object-cover "/>
+      </div>
+      <h3 className="font-fajila text-xl mt-4 text-[#5bafa5]">{item.item_name}</h3>
+      <div className="flex justify-between items-center mt-4">
+        <p className="flex gap-3 items-center font-fajila text-[18px] "><GrMoney /> {price}</p>
+        <p className="flex gap-3 items-center font-fajila text-[18px] text-[#5bafa5]"><GiTimeTrap /> {processing_time}</p>
+      </div>
+      <div className="flex items-center justify-between mt-3 my-5">
+        <p className="flex gap-3 items-center font-fajila text-[18px] text-[#5bafa5]"> <IoStarHalf /> {rating}</p>
+        <p className="flex gap-3 items-center font-fajila text-[18px]  "> <SiShutterstock />{stockStatus}</p>
+      </div>
+      <div className="my-4 flex items-center justify-center">
+      <Link className="py-3 px-6 flex items-center gap-5 font-bold font-lobister text-xl hover:text-white hover:bg-[#5bafa5] rounded-lg border border-[#5bafa5]">View Details <FaLocationArrow /> </Link>
+      </div>
+    </div>
+  );
+};
+
+export default ArtAndCraftCard;
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,18 @@
 import Banner from "../components/Banner";
 import NewsLetter from "../components/NewsLetter";
 import photo from '../../public/Wave.svg'
+import { useEffect, useState } from "react";
+import ArtAndCraftCard from "../components/ArtAndCraftCard";
+
 
 const HomePage = () => {
+  const [artCollection, setArtCollection] = useState([])
+useEffect(()=>{
+  fetch('http://localhost:5000/artCollection')
+  .then(res=> res.json())
+  .then(data=> setArtCollection(data))
+},[])
+
   return (
     <div className="w-full mx-auto relative">
 <div className="absolute -top-10 left-0 w-full">
@@ -10,6 +20,14 @@ const HomePage = () => {
   </div>
   <Banner></Banner>
       <NewsLetter></NewsLetter>
+      <div className="px-24 my-10 ">
+        <h3 className="font-extrabold text-5xl font-fajila border-b-2 w-fit pb-3 text-[#5bafa5]">Browse Our Collection</h3>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5 mt-5">
+      {
+        artCollection.slice(0, 6).map(item => <ArtAndCraftCard key={item._id} item={item}></ArtAndCraftCard>)
+      }
+      </div>
+      </div>
       <img src={photo} alt="" className='max-h-72 w-full'/>
     </div>
   );
